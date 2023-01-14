2263a9ff307fb1b87b168114fa01195940916868
Dhruv Bansal <dhruvtgs003@gmail.com>
2023-01-14 23:15:23 +0530

    home

--- guardian/lib/main.dart
+++ guardian/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guardian/home.dart';
 import 'login_page.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: Home(),
     );
   }
 }
