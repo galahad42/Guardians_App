dbbd9bc3ea8c43d305611fbaa8d7ed81092bdc55
Amrit Anand <amritanand.dev@gmail.com>
2023-01-16 16:58:14 +0530

    Revert "Dhruv"

--- guardian/lib/main.dart
+++ guardian/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:guardian/home.dart';
 import './LoginScreen/login_page.dart';
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: LoginPage(),
     );
   }
 }
